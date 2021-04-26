e43037b580a4ff6e71b98ad2650c6ac198e055be
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-25 23:33:28 -0400

    commit tree 8f0e6074c0d205be426aef414e0df4a6e8b1b6dd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+  fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(toys) {
+
+      let toysHTML = toys.map(function(toy) {
+        return `
+         <div class="card">
+          <h2>${toy.name}</h2>
+          <img src=${toy.image} class="toy-avatar" />
+          <p>${toy.likes} Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div>
+        `
+      })
+      
+
+      document.querySelector("#toy-collection").innerHTML += toysHTML
+      
+    })
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
